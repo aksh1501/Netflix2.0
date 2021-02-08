05df872da98cf848a4c489bd49a5a29c784a4f36
akshit.s <akshit.s@BBMDVAKSHITS.local>
2021-02-08 12:44:10 +0530

    new  commit

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-function whole()
-{
+import {GetMovieInfo} from "./movieData.js";
+import {debounce,searchImplementation} from './debounceSearchImpl.js';
 
-  fetch('https://demo0376970.mockable.io/movieslist',{
+const ApiKey="109daa35";
+let movieDetails=new Map();
+
+fetch('https://demo0376970.mockable.io/movieslist',{
     method: "GET"
   })
   .then(
@@ -24,63 +27,24 @@ function whole()
   });
 
 
-const ApiKey="109daa35";
-
-let showTitle,releaseDate,description;
-
-let movieDetails=new Map();
-
 function createMapping(data){
   const allShows=data["shows"];
-
-  // console.log(allShows);
-
-  allShows.forEach((show)=>{
-    // console.log(show['imdbID']);
+    allShows.forEach((show)=>{
     GetMovieInfo(ApiKey ,show['imdbID'],movieDetails);
   });
-  // console.log(movieDetails);
+  
 }
 
-function GetMovieInfo(api,id,cache){
 
-  let url='http://www.omdbapi.com/?apikey='+api+'&i='+id;
-  fetch(url,{
-    method: "GET"
-  })
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
+function entirePage(objectFromJson)
+{
+  let searchKey=document.getElementsByClassName("my-input")[0]
 
-      // Examine the text in the response
-      response.json().then(function(data) {
-        // console.log(id,data);
-        // console.log(cache);
-        cache[id]=data;
-      });
-    }
-  )
-  .catch(function(err) {
-    console.log("idk");
-    console.log('Fetch Error :-S', err);
-  });
+  const showData=objectFromJson['shows'];
 
-}
-  // console.log(varbl);
-
-
-
-
-
-function entirePage(objectFromJson){
-
-    function showTemplate(show) 
-    {
-        return `<li class="flex-item">
+  function showTemplate(show) 
+  {
+      return `<li class="flex-item">
             <article class="showContent">
             <img class="showImage" src="./img/posters/${show.poster}">
             <h3 class="showTitle">${show.title}</h3>
@@ -95,32 +59,27 @@ function entirePage(objectFromJson){
 
   function handleClick()
   {
-    console.log(movieDetails);
-    // console.log(arguments[0]["target"].getElementsByTagName('h6')[0].innerText);
     const imdbId=arguments[0]["target"].getElementsByTagName('h5')[0].innerText;
-    // console.log(imdbId);
-    // const showTitle=arguments[0]["target"].getElementsByTagName('h3')[0].innerHTML; 
+    
     const showTitle=movieDetails[imdbId]['Title'];  
-    // const releaseDate=arguments[0]["target"].getElementsByTagName('h4')[0].innerHTML; 
+     
     const releaseDate=movieDetails[imdbId]['Released']
-    // const description=arguments[0]["target"].getElementsByTagName('p')[0].innerHTML;  
+     
     const description=movieDetails[imdbId]['Plot'];  
     
     const imagePath=arguments[0]["target"].getElementsByTagName('img')[0].src;
     
     const trailer=arguments[0]["target"].getElementsByTagName('h6')[0].innerText;
 
-    console.log(trailer);
-
     const trailerUrl="https://www.youtube-nocookie.com/embed/"+trailer;
 
     const rating=movieDetails[imdbId]['Ratings'][0].Value;
 
-    // console.log(movieDetails[imdbId]['Ratings'][0]);
-    // console.log(blk.getElementsByTagName("h3")[0].innerHTML,"clicked");
     const pageView=document.getElementsByClassName("flex-container")[0];
    
     pageView.classList.add("noShow");
+
+    searchKey.classList.add("noShow");
 
     document.getElementById("entireContent").innerHTML=`
     <ul class="moviePage">
@@ -132,85 +91,34 @@ function entirePage(objectFromJson){
       </li>
       <li>${description}</li>
     </div>
-    <p align="center"><iframe class="trailerVideo" src=${trailerUrl} fullscreen=""></iframe></p>`;
+    <p align="center"><iframe class="trailerVideo" src=${trailerUrl}></iframe></p>`;
   
   }
 
-  
-  const showData=objectFromJson['shows'];
-
-//adding a better naming for the id of search element
-document.getElementById("entireContent").innerHTML=`<ul id="myul" class="flex-container">
-${showData.map(showTemplate).join("")}
-</ul>`;
-
-const movieBlock=document.querySelectorAll(".flex-item");
-
-// console.log(movieBlock);
-
-movieBlock.forEach((blk)=>{
-  // console.log(blk.getElementsByTagName("h3")[0].innerHTML);
-  // console.log(blk);
-  blk.removeEventListener('click',handleClick);
-  blk.addEventListener('click',handleClick);
-});
-
-//adding event handler in the javascript file
-let searchKey=document.getElementsByClassName("my-input")[0];
-
-let searchImplementation=function(){
-    let inputText, filteredText, ulElementData, liElementData, idx, aElementData, eachShowTitle;
-
-        inputText = document.getElementsByClassName("my-input")[0];
-        // console.log(input);
-        filteredText = inputText.value.toUpperCase();
-        ulElementData = document.getElementById("myul");
-        liElementData = ulElementData.getElementsByTagName("li");
-
-        for (idx = 0; idx < liElementData.length; idx++) {
-            aElementData = liElementData[idx].getElementsByTagName("h3")[0];
-            eachShowTitle = aElementData.textContent || aElementData.innerText;
-
-            if (eachShowTitle.toUpperCase().indexOf(filteredText) > -1) {
-                liElementData[idx].style.display = "";
-            }
-            else {
-                liElementData[idx].style.display = "none";
-            }
-        }
-    }
-
-    function debounce(func, wait) {
-        let timeout;
-        return function debounced() { 
-            var context = this;
-            console.log(context);
-            var args = arguments;
-      
-            function later() {
-              func.apply(context, args);
-            }
-            if (timeout) {
-              clearInterval(timeout);
-            }
-            timeout = setTimeout(later, wait);
-        }
-      }
-
-    var debouncedSearch=debounce(searchImplementation,700);
 
 
-    searchKey.oninput=() => {
-        let textEntered=searchKey.value;
+  //adding a better naming for the id of search element
+  document.getElementById("entireContent").innerHTML=`<ul id="myul" class="flex-container">
+  ${showData.map(showTemplate).join("")}
+  </ul>`;
+
+  const movieBlock=document.querySelectorAll(".flex-item");
+
+  movieBlock.forEach((blk)=>{
+    blk.removeEventListener('click',handleClick);
+    blk.addEventListener('click',handleClick);
+  });
+
+
+  let debouncedSearch=debounce(searchImplementation,700);
+
+  searchKey.oninput=() => {
+      let textEntered=searchKey.value;
         
-        if(textEntered!=='' && textEntered.length>=2)
-        {
-            debouncedSearch();
-        }
-
+      if(textEntered!=='' && textEntered.length>=2)
+      {
+          debouncedSearch();
+      }
     }
 }
 
-}
-
-whole();
---
+++ debounceSearchImpl.js
@@ -0,0 +1,37 @@
+export let searchImplementation=function(){
+    let inputText, filteredText, ulElementData, liElementData, idx, aElementData, eachShowTitle;
+
+        inputText = document.getElementsByClassName("my-input")[0];
+        // console.log(input);
+        filteredText = inputText.value.toUpperCase();
+        ulElementData = document.getElementById("myul");
+        liElementData = ulElementData.getElementsByTagName("li");
+
+        for (idx = 0; idx < liElementData.length; idx++) {
+            aElementData = liElementData[idx].getElementsByTagName("h3")[0];
+            eachShowTitle = aElementData.textContent || aElementData.innerText;
+
+            if (eachShowTitle.toUpperCase().indexOf(filteredText) > -1) {
+                liElementData[idx].style.display = "";
+            }
+            else {
+                liElementData[idx].style.display = "none";
+            }
+        }
+    }
+
+export function debounce(func, wait) {
+    let timeout;
+    return function debounced() { 
+        var context = this;
+        var args = arguments;
+  
+        function later() {
+          func.apply(context, args);
+        }
+        if (timeout) {
+          clearInterval(timeout);
+        }
+        timeout = setTimeout(later, wait);
+    }
+  }
